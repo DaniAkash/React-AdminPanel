a8d23c52c8a6f2ecd6a6ba2a48202e2eb56bca3f
DaniAkash <eaglearrow.arrow@gmail.com>
2016-09-24 22:13:06 +0530

    back to hash locations

--- src/main.js
+++ src/main.js
@@ -4,6 +4,6 @@ var React = require('react');
 var Router = require('react-router');
 var routes = require('./routes');
 
-Router.run(routes, Router.HistoryLocation, function(Handler) {
+Router.run(routes, function(Handler) {
 	React.render(<Handler/>, document.getElementById('app'));
 });
