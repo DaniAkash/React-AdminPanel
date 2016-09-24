0510aa80f77b45faf7a4bff4aeca74c182c0c2ff
DaniAkash <eaglearrow.arrow@gmail.com>
2016-09-24 22:02:26 +0530

    History Location

--- src/main.js
+++ src/main.js
@@ -4,6 +4,6 @@ var React = require('react');
 var Router = require('react-router');
 var routes = require('./routes');
 
-Router.run(routes, function(Handler) {
+Router.run(routes, Router.HistoryLocation, function(Handler) {
 	React.render(<Handler/>, document.getElementById('app'));
 });
