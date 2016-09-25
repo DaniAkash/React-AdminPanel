a9404c224f48266679d80a4d5b34df33c74a0f42
DaniAkash <eaglearrow.arrow@gmail.com>
2016-09-26 00:05:31 +0530

    website without flux

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express'),
+    app = express(),
+    path = require('path');
+
+app.use(express.static('dist'));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname+'/dist/index.html'));
+});
+
+app.listen(8000);
